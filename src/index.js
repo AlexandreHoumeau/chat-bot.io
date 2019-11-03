cb119a8b70709e2ed196236417b9ffb1e5c614a5
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2019-11-03 23:39:05 +0100

    chore(script): create and edit script file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-class HelloWorld {
+import './index.scss';
+
+class ChatBot {
   constructor() {
-    this.say = 'Hello World';
+    this.addMsg = document.forms['add-msg'];
+    this.msgContainer = document.getElementById('ownMsg');
+    this.elBody = document.querySelector('body');
   }
 
-  speak(say = this.say) {
-    console.log(say);
+  speak(addMsg = this.addMsg) {
+    addMsg.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const { value } = addMsg.querySelector('input[type="text');
+      console.log(value);
+      this.storingMsg(value);
+    });
+  }
+
+  storingMsg(value) {
+    const elDiv = document.createElement('div');
+    const elDivMsg = document.createElement('div');
+
+    elDiv.classList.add('d-flex');
+    elDiv.classList.add('mb-4');
+    elDiv.classList.add('justify-content-end');
+
+    elDivMsg.textContent = value;
+    elDivMsg.classList.add('msg_cotainer_send');
+    elDiv.appendChild(elDivMsg);
+    this.msgContainer.append(elDiv);
   }
 }
 
-const helloWordl = new HelloWorld();
+const chatBot = new ChatBot();
 
-helloWordl.speak();
-helloWordl.speak('Bonjour le monde');
-helloWordl.speak('Hello GAB');
+chatBot.speak();
