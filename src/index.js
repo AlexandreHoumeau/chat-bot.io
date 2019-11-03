7ffe471043cc7619411a4e09bf13a482c47c1957
AlexandreHoumeau <alexandre.houmeau@gmail.com>
2019-11-03 23:33:27 +0100

    chore(index): create and add the html final context

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello World');
+class HelloWorld {
+  constructor() {
+    this.say = 'Hello World';
+  }
+
+  speak(say = this.say) {
+    console.log(say);
+  }
+}
+
+const helloWordl = new HelloWorld();
+
+helloWordl.speak();
+helloWordl.speak('Bonjour le monde');
+helloWordl.speak('Hello GAB');
